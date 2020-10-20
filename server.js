a229fa133bc155c6bbd2c42fa8842973401cc81d
sureshmangs <sureshmangs1@gmail.com>
2020-10-20 11:28:21 +0530

    Updating routes

--- server.js
+++ server.js
@@ -6,16 +6,41 @@ const path = require('path');
 const app = express();
 
 
+
 app.use(express.static('public'));
+
+
+
+// setting template engine
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+
+
+// Routes
 
 app.get('/', (req, res) => {
     res.render('homepage');
-})
+});
 
-// setting template engine
-app.use(expressLayouts);
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+});
+
+
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
