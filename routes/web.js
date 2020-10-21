99f10b19c7f78194cd609fbef62b1bc10e4037ab
sureshmangs <sureshmangs1@gmail.com>
2020-10-21 21:39:59 +0530

    updating routes

--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,22 @@
 const homepageController = require('../app/helpers/controllers/homepageController');
 const authController = require('../app/helpers/controllers/authController');
 const cartController = require('../app/helpers/controllers/customers/cartController');
+const guest = require('../app/helpers/middlewares/guest');
 
 function initRoutes(app) {
     app.get('/', homepageController().index);
 
     app.get('/cart', cartController().cart);
 
-    app.get('/register', authController().register);
+    app.get('/register', guest, authController().register);
 
-    app.get('/login', authController().login);
+    app.post('/register', authController().postRegister);
+
+    app.get('/login', guest, authController().login);
+
+    app.post('/login', authController().postLogin);
+
+    app.post('/logout', authController().logout);
 
     app.post('/update-cart', cartController().update);
 }
