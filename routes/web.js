b15484564ef3c144f357dbb6c3125749154702e9
sureshmangs <sureshmangs1@gmail.com>
2020-10-22 21:03:16 +0530

    updating routes

--- routes/web.js
+++ routes/web.js
@@ -1,24 +1,38 @@
 const homepageController = require('../app/helpers/controllers/homepageController');
 const authController = require('../app/helpers/controllers/authController');
 const cartController = require('../app/helpers/controllers/customers/cartController');
+const orderController = require('../app/helpers/controllers/customers/orderController');
+const adminOrderController = require('../app/helpers/controllers/admin/orderController');
+
+// Middlewares
 const guest = require('../app/helpers/middlewares/guest');
+const auth = require('../app/helpers/middlewares/auth');
+const admin = require('../app/helpers/middlewares/admin');
+
+
+
 
 function initRoutes(app) {
+    // homepage routes
     app.get('/', homepageController().index);
 
+    // cart routes
     app.get('/cart', cartController().cart);
+    app.post('/update-cart', cartController().update);
 
+    // Auth routes
     app.get('/register', guest, authController().register);
-
     app.post('/register', authController().postRegister);
-
     app.get('/login', guest, authController().login);
-
     app.post('/login', authController().postLogin);
-
     app.post('/logout', authController().logout);
 
-    app.post('/update-cart', cartController().update);
+    // order routes
+    app.post('/orders', auth, orderController().store);
+    app.get('/customer/orders', auth, orderController().orders);
+
+    // Admin routes
+    app.get('/admin/orders', admin, adminOrderController().orders);
 }
 
 module.exports = initRoutes;
