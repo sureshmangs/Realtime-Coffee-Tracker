adcb9179633c26096ee8875ea68fed01bba7cb07
sureshmangs <sureshmangs1@gmail.com>
2020-10-22 21:02:41 +0530

    updating views

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Noty from 'noty';
+import { initAdmin } from './admin';
 
 let addToCart = document.querySelectorAll('.add-to-cart');
 let cartCounter = document.querySelector('#cartCounter');
@@ -35,3 +36,18 @@ addToCart.forEach((btn) => {
         updateCart(coffee);
     })
 })
+
+
+
+// Remove alert message displayed in orders after 2 seconds
+const alertMsg = document.querySelector('#success-alert')
+if (alertMsg) {
+    setTimeout(() => {
+        alertMsg.remove()
+    }, 2000)
+}
+
+
+
+
+initAdmin();   // calling admin orders code
