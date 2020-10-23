b4a12f8ddace209a587e79340a6e983fab42f930
sureshmangs <sureshmangs1@gmail.com>
2020-10-23 22:42:00 +0530

    Updating views

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import dayjs from 'dayjs';
+import Noty from 'noty';
 
 
-export function initAdmin() {
+export function initAdmin(socket) {
     const orderTableBody = document.querySelector('#orderTableBody')
     let orders = []
     let markup
@@ -12,7 +13,6 @@ export function initAdmin() {
             "X-Requested-With": "XMLHttpRequest"
         }
     }).then(res => {
-        console.log('calling via axios')
         orders = res.data
         markup = generateMarkup(orders)
         orderTableBody.innerHTML = markup
@@ -41,7 +41,7 @@ export function initAdmin() {
                 <td class="border px-4 py-2">${order.address}</td>
                 <td class="border px-4 py-2">
                     <div class="inline-block relative w-64">
-                        <form action="/admin/order/status" method="POST">
+                        <form action="/admin/orders/status" method="POST">
                             <input type="hidden" name="orderId" value="${order._id}">
                             <select name="status" onchange="this.form.submit()"
                                 class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
@@ -77,4 +77,19 @@ export function initAdmin() {
         `
         }).join('')
     }
+
+    // socket client
+
+    socket.on('orderPlaced', (order) => {
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'New Order',
+            progressBar: false,
+        }).show();
+        orders.unshift(order);
+        orderTableBody.innerHTML = '';
+        orderTableBody.innerHTML = generateMarkup(orders);
+    })
+
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Noty from 'noty';
 import { initAdmin } from './admin';
+import dayjs from 'dayjs';
 
 let addToCart = document.querySelectorAll('.add-to-cart');
 let cartCounter = document.querySelector('#cartCounter');
@@ -9,7 +10,6 @@ let cartCounter = document.querySelector('#cartCounter');
 function updateCart(coffee) {
     axios.post('/update-cart', coffee)
         .then(res => {
-            console.log(res);
             cartCounter.innerText = res.data.totalQty;
             new Noty({
                 type: 'success',
@@ -50,4 +50,69 @@ if (alertMsg) {
 
 
 
-initAdmin();   // calling admin orders code
+
+
+// changing order status
+let hiddenInput = document.querySelector('#hiddenInput');
+let statuses = document.querySelectorAll('.status_line');
+let order = hiddenInput ? hiddenInput.value : null;
+
+let time = document.createElement('small');
+
+function updateStatus(order) {
+    let stepCompleted = true;
+    statuses.forEach((status) => {
+        status.classList.remove('step-completed');
+        status.classList.remove('current');
+    })
+    statuses.forEach((status) => {
+        let dataProp = status.dataset.status;
+        if (stepCompleted) {
+            status.classList.add('step-completed');
+        }
+
+        if (dataProp == order.status) {
+            stepCompleted = false;
+            time.innerText = dayjs(order.updateAt).format('MMM D, YYYY h:mm A');
+            status.appendChild(time)
+            if (status.nextElementSibling) {
+                status.nextElementSibling.classList.add('current')
+            }
+        }
+
+    })
+}
+
+updateStatus(JSON.parse(order));
+
+
+// socket client
+
+let socket = io();
+
+initAdmin(socket);   // calling admin orders code
+
+if (order) {
+    socket.emit('join', `order_${JSON.parse(order)._id}`)
+}
+
+socket.on('orderUpdated', (data) => {
+    const updatedOrder = { ...order };
+    updatedOrder.updateAt = dayjs().format();
+    updatedOrder.status = data.status;
+    updateStatus(updatedOrder);
+
+    new Noty({
+        type: 'success',
+        timeout: 1000,
+        text: 'Order Updated',
+        progressBar: false,
+    }).show();
+})
+
+
+let adminAreaPath = window.location.pathname;
+
+if (adminAreaPath.includes('admin')) {
+    socket.emit('join', 'adminRoom');
+}
